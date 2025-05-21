784a75327d44944764571aa60906122201e8d9a3
vini-21samuel <viniciussamuel1277@gmail.com>
2025-05-20 22:40:37 -0300

    Primeira API REST com Spring Boot - Hello World

---
+++ src/main/java/med/voll/api/HelloController.java
@@ -0,0 +1,17 @@
+package med.voll.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/opa")
+
+public class HelloController {
+    @GetMapping
+    public String olaMundo(){
+        return "Olá Mundo";
+    }
+}
